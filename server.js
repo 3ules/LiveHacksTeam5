b111b84729965dc2e6569145dd203bb507738a17
babblet <frk.haha@hotmail.se>
2019-08-31 20:23:23 +0200

    reslove conflict

--- server.js
+++ server.js
@@ -30,24 +30,44 @@ let state = {
     }
 }
 
-const game_init = () => {
+const wp_send = (type, json) => {
+    ws.send(JSON.stringify({
+        type,
+        ...json,
+    }));
+}
 
-    ws.send(JSON.stringify({state: state.game.state}))
+const wp_user_send = (command, string) => {
+    ws.send(command);
+    ws.send(string);
+}
+
+const wait_for_users = () => {
+
+}
+
+const game_start = () => {
+    if(state.game.state === 'start') {
+        wp_user_send('game', 'choices');
+    }
 }
 
 const handleMessage = (m) => {
-    if(m.type === 'auth' && m.user) {
-        state.users[m.user.id] = {
+    if(m.type === 'name' && m.user_id) {
+        state.users[m.user_id] = {
             name: m.user.name,
             guess: undefined,
             state: 'authenticated',
             score: 0,
         }
         console.log(Object.keys(state.users));
+    } else if(m.type === 'user_number' && m.user_id && state.game.state) {
+        state.users[m.user_id].number = m.number;
     } else if (m.type === 'game_command' && m.command) {
-        if(command === 'start' && state.game.state === 'new') {
+        if(m.command === 'start' && state.game.state === 'new') {
             state.game.state === 'start',
-            game_init();
+            wp_user_send('command: game', 'start');
+            game_start();
         }
     } else if (m.type === 'user_guess' && m.user) {
         if(state.users[m.user.id]) {
