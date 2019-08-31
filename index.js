4b7ff93493593c9dbc7fda46cfa164875f3228a4
babblet <frk.haha@hotmail.se>
2019-08-31 16:06:57 +0200

    More things...

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
 const WebSocket = require('ws');
 
-
-const eventName = 'MindReader';
-
-const ws = new WebSocket(`ws://stagecast.se/api/events/${eventName}/ws?x-user-listener=1`);
-
-const USER = JSON.stringify({
+const EVENT_NAME = 'MindReader';
+const ws = new WebSocket(`ws://stagecast.se/api/events/${EVENT_NAME}/ws?x-user-listener=1`);
+const SERVER_USER = JSON.stringify({
     email: "fkarlsson@solidsport.com",
     password: "Team5Team5",
 });
-let STATE = {
-    AUTHENTICATED: undefined,
-    USERS: {},
-    GAME: {}
+
+
+// users states
+// 'ready'
+// 'waiting'
+// 'quit'
+// ''
+
+
+// Game states
+// new
+// guess
+// reveal
+// result
+
+let state = {
+    authenticated: undefined,
+    users: {},
+    game: {
+        state: "new",
+        user_scores: {},
+    }
+}
+
+const game_init = () => {
+
+    ws.send(JSON.stringify({state: state.game.state}))
 }
 
 const handleMessage = (m) => {
     if(m.type === 'auth' && m.user) {
-        STATE.USERS[m.user.id] = {
+        state.users[m.user.id] = {
             name: m.user.name,
             guess: undefined,
-            //image: undefined,
+            state: 'authenticated',
+            score: 0,
         }
-        console.log(STATE.USERS);
+        console.log(Object.keys(state.users));
+    } else if (m.type === 'game_command' && m.command) {
+        if(command === 'start' && state.game.state === 'new') {
+            state.game.state === 'start',
+            game_init();
+        }
+    } else if (m.type === 'user_guess' && m.user) {
+        if(state.users[m.user.id]) {
+            state.users[m.user.id].guess = m.user.guess
+        }
     }
 };
 
 ws.on('open', () => {
-    ws.send(USER);
+    ws.send(SERVER_USER);
 });
 
 ws.on('message', data => {
 	try {
         if(data.toString() === "ok") {
-            STATE.AUTHENTICATED = true;
+            state.authenticated = true;
             console.log('Authenticated');
         } else {
             data = JSON.parse(data);
@@ -43,10 +73,6 @@ ws.on('message', data => {
         console.log(e);
     }
 });
-
-Number.prototype.map = function (in_min, in_max, out_min, out_max) {
-	return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-};
 
 process.on('SIGINT', () => {
 	output.closePort();
